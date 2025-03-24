4388ff5e9f08a5a675772b6631717f83e083b0f7
Muhammad Asmatullah <masmatullah450@gmail.com>
2025-03-24 09:44:00 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace UserGreetingProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}! Welcome to the C# world.");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+
